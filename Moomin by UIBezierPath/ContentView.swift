0a56b066c23ded0d24aaff07b61080a8fe858498
Ryan CY <phillin820109@msn.com>
2022-08-12 23:55:30 +0800

    Initial Commit

---
+++ Moomin by UIBezierPath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moomin by UIBezierPath
+//
+//  Created by Ryan Lin on 2022/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moomin by UIBezierPath/Moomin_by_UIBezierPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moomin_by_UIBezierPathApp.swift
+//  Moomin by UIBezierPath
+//
+//  Created by Ryan Lin on 2022/8/12.
+//
+
+import SwiftUI
+
+@main
+struct Moomin_by_UIBezierPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
